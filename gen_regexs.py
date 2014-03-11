6d6190a39f54739ec4b6cb126ef2068a99806b3f
Newky <richdel1991@gmail.com>
2014-03-11 22:26:01 +0000

    Got it running pretty efficiently now.

---
+++ gen_regexs.py
@@ -0,0 +1,16 @@
+import sys
+
+def get_regex(word):
+   regexs = []
+   for i in range(0, len(word)-4):
+     for j in range(i+4, len(word)+1):
+       regexs.append(word[i:j])
+   return regexs
+
+def make_regex(word):
+    return "(%s)" % ("|".join(get_regex(word)))
+
+
+if __name__ == "__main__":
+    for word in sys.stdin:
+        print make_regex(word.strip())
--- grepper.sh
+++ grepper.sh
@@ -1,24 +1,5 @@
 #!/bin/bash
-
-DICTIONARY="/usr/share/dict/words"
-#DICTIONARY="test.txt"
-
-words=""
-while read input; do
-    words="$words $input"
+for line in `cat | python gen_regexs.py`; do
+    egrep "^$line$" /usr/share/dict/words
 done
-
-echo "$words"
-
-count=0
-for word in `grep '^...' $DICTIONARY`; do
-    newlineremoved_word="`printf "%s" $word`"
-    echo $words | grep -q "$newlineremoved_word";
-    if [ $? -eq 0 ]; then
-        echo `echo -n $word`
-    fi
-    if [ $(( count % 10000 )) == 0 ]; then
-        echo "Count is $count."
-    fi
-    count=$(( count + 1 ));
-done
+exit 0
